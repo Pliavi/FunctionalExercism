99c313984b4ba814a4595d227a2bc2d2fd6ae538
Pliavi <pliavi.rpgm@gmail.com>
2018-01-07 19:24:31 -0200

    added raindrops

---
+++ elixir/raindrops/raindrops.exs
@@ -0,0 +1,25 @@
+defmodule Raindrops do
+  @doc """
+  Returns a string based on raindrop factors.
+
+  - If the number contains 3 as a prime factor, output 'Pling'.
+  - If the number contains 5 as a prime factor, output 'Plang'.
+  - If the number contains 7 as a prime factor, output 'Plong'.
+  - If the number does not contain 3, 5, or 7 as a prime factor,
+    just pass the number's digits straight through.
+  """
+  @factors %{3 => "Pling", 5 => "Plang", 7 => "Plong"}
+
+  @spec convert(pos_integer) :: String.t
+  def convert(number) do
+    @factors
+    |> Enum.map(
+      fn({n, sound}) -> 
+        if (rem(number, n) == 0), 
+          do: sound, 
+          else: "" 
+      end)
+    |> List.to_string
+    |> (&Kernel.if(&1=="", do: "#{number}", else: &1)).()
+    end
+end
